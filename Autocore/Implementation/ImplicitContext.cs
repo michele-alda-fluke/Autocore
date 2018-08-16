e0941c8cbb75ee4675e1227667e25d4fae97da0b
Michele Alda <michele.alda@emaint.com>
2018-08-16 11:42:12 +0100

    Add support for netstandard2.0

--- Autocore/Implementation/ImplicitContext.cs
+++ Autocore/Implementation/ImplicitContext.cs
@@ -20,8 +20,10 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 // 
+
+using System.Collections.Concurrent;
 using Autocore.Interfaces;
-using System.Runtime.Remoting.Messaging;
+using System.Threading;
 
 namespace Autocore.Implementation
 {
@@ -37,8 +39,34 @@ namespace Autocore.Implementation
 
 		/// <see cref="Autocore.Interfaces.IImplicitContext.Container"/>
 		public IVolatileContainer Container {
-			get { return CallContext.LogicalGetData(SCOPE_KEY) as IVolatileContainer; }
-			set { CallContext.LogicalSetData(SCOPE_KEY, value); }
+			get => CallContext.GetData(SCOPE_KEY) as IVolatileContainer;
+		    set => CallContext.SetData(SCOPE_KEY, value);
 		}
 	}
+
+    /// <summary>
+    /// Provides a way to set contextual data that flows with the call and 
+    /// async context of a test or invocation.
+    /// </summary>
+    public static class CallContext
+    {
+        static ConcurrentDictionary<string, AsyncLocal<object>> state = new ConcurrentDictionary<string, AsyncLocal<object>>();
+
+        /// <summary>
+        /// Stores a given object and associates it with the specified name.
+        /// </summary>
+        /// <param name="name">The name with which to associate the new item in the call context.</param>
+        /// <param name="data">The object to store in the call context.</param>
+        public static void SetData(string name, object data) =>
+            state.GetOrAdd(name, _ => new AsyncLocal<object>()).Value = data;
+
+        /// <summary>
+        /// Retrieves an object with the specified name from the <see cref="CallContext"/>.
+        /// </summary>
+        /// <param name="name">The name of the item in the call context.</param>
+        /// <returns>The object in the call context associated with the specified name, or <see langword="null"/> if not found.</returns>
+        public static object GetData(string name) =>
+            state.TryGetValue(name, out AsyncLocal<object> data) ? data.Value : null;
+    }
+
 }
